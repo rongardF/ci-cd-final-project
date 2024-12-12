f4fec86a32268bbe4bf4971d603c0223b81b0f9c
rongardF <ronfreimann@gmail.com>
2024-12-12 10:44:05 -0500

    blablaa

--- service/routes.py
+++ service/routes.py
@@ -41,7 +41,7 @@ def list_counters():
     app.logger.info("Request to list all counters...")
 
     counters = [
-        dict(name=count[0], counter=count[1]) 
+        dict(name=count[0], counter=count[1])
         for count in COUNTER.items()
     ]
 
@@ -58,7 +58,7 @@ def create_counters(name):
 
     if name in COUNTER:
         return abort(
-            status.HTTP_409_CONFLICT, 
+            status.HTTP_409_CONFLICT,
             f"Counter {name} already exists"
         )
 
@@ -82,7 +82,7 @@ def read_counters(name):
 
     if name not in COUNTER:
         return abort(
-            status.HTTP_404_NOT_FOUND, 
+            status.HTTP_404_NOT_FOUND,
             f"Counter {name} does not exist"
         )
 
@@ -100,7 +100,7 @@ def update_counters(name):
 
     if name not in COUNTER:
         return abort(
-            status.HTTP_404_NOT_FOUND, 
+            status.HTTP_404_NOT_FOUND,
             f"Counter {name} does not exist"
         )
 
